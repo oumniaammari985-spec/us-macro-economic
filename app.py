28a7f125c95c8139d19cc506608ef5a21d96f402
oumniaammari985-spec <oumniaammari985@gmail.com>
2025-10-25 23:09:25 +0100

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("U.S Macro Economic Dashboard")
+
+st.write("Welcome! This app will display automatic macroeconomic data.")
+
+# مثال ديال البيانات
+st.subheader("GDP")
+st.write("GDP data will appear here automatically.")
